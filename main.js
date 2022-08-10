378dab273eeadaa8520ef551916c22505f16a5b4
eliseuSousa <eliseusousa759@gmail.com>
2022-08-10 11:35:03 -0300

    Validando formulario

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
 (() => {
 
   const init = () => {
+
+    const validateEmail = (event) => {
+      const input = event.target;
+      const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+      const emailTest = regex.test(input.value);
+
+      if(!emailTest) {
+        buttonSubmit.setAttribute('disabled', 'disabled');
+        input.nextElementSibling.classList.add('error');
+      } else {
+        buttonSubmit.removeAttribute('disabled');
+        input.nextElementSibling.classList.remove('error');
+      }
+    }
+
+    const validatePassword = (event) => {
+      const input = event.currentTarget;
+
+      if(input.value.length < 8) {
+        buttonSubmit.setAttribute('disabled', 'disabled');
+        input.nextElementSibling.classList.add('error');
+      } else {
+        buttonSubmit.removeAttribute('disabled', 'disabled');
+        input.nextElementSibling.classList.remove('error');
+      }
+    }
+
     const inputEmail = document.querySelector('input[type="email"]');
     const inputPassword = document.querySelector('input[type="password"]');
     const buttonSubmit = document.querySelector('.submit');
 
+    inputEmail.addEventListener('input', validateEmail);
+    inputPassword.addEventListener('input', validatePassword);
+
+    const errorHandle = () => {
+      buttonSubmit.classList.remove('success');
+      buttonSubmit.classList.add('error');
+      buttonSubmit.textContent = 'Erro :(';
+    }
+
+    const successHandle = () => {
+      buttonSubmit.classList.remove('error');
+      buttonSubmit.classList.add('success');
+      buttonSubmit.textContent = 'Sent ;)';
+    }
+
     if(buttonSubmit) {
       buttonSubmit.addEventListener('click', (event) => {
         event.preventDefault();
+
+        buttonSubmit.textContent = '... Loading';
 
         fetch('https://reqres.in/api/login', {
           method: 'POST',
@@ -19,9 +63,13 @@
             password: inputPassword.value,
           })
         }).then((response) => {
-          return response.json();
-        }).then((data) => {
-          console.log(data);
+          if(response.status != 200) {
+            errorHandle();
+            return;
+          }
+          successHandle();
+        }).catch(() => {
+          errorHandle();
         })
         
       });
